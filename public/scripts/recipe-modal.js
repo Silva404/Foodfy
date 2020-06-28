b24e0b33d4658ad749ab27184ae4f38b57689882
Silva404 <brenosilva0252@gmail.com>
2020-06-28 17:13:06 -0300

    challenge 1 finished

--- public/scripts/recipe-modal.js
+++ public/scripts/recipe-modal.js
@@ -1,8 +1,15 @@
-const modal = document.querySelector('.modal-content')
-const cards = document.querySelectorAll('.cards-container')
+const modal = document.querySelector('.modal')
+const modalContent = document.querySelector('.modal-content')
+const cards = document.querySelectorAll('.card')
+const close = document.querySelector('.close')
 
 for (let card of cards) {
     card.addEventListener('click', e => {
         modal.classList.remove('hide')
+        modalContent.innerHTML = card.innerHTML        
     })
 }
+
+close.addEventListener('click', e => {
+    modal.classList.toggle('hide')
+})
