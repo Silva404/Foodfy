c2ca553684e2e360af0c4c56050e49de20eac2f0
Silva404 <brenosilva0252@gmail.com>
2020-06-30 01:16:59 -0300

    hide and show added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,4 @@
 const cards = document.querySelectorAll(".card")
-const hideButton = document.querySelector('.hide')
 
 for (let card of cards) {
   card.addEventListener("click", (e) => {
@@ -9,8 +8,20 @@ for (let card of cards) {
   });
 }
 
-hideButton.addEventListener('click', e => {
-  if (hideButton.innerHTML == 'esconder'){
-    
-  }
-})
+
+const description = document.getElementsByClassName('recipe-description')
+
+for (let i = 0; i < description.length; i++) {
+  const span = description[i].querySelector('.span')
+  const content = description[i].querySelector('.description')
+
+  span.addEventListener('click', e => {
+    if (span.innerHTML == 'esconder'){
+      span.innerHTML = 'mostrar'
+      content.classList.add('hide')
+    } else {
+      span.innerHTML = 'esconder'
+      content.classList.remove('hide')
+    }
+  })
+}
