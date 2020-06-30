8b935cf3f8153a9aab72efd94fe3601c599b4275
Silva404 <brenosilva0252@gmail.com>
2020-06-30 00:13:25 -0300

    added id

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
 const data = require("./data");
-const routes = require("./routes");
+const routes = require("./routes")
 
 const server = express();
 
 server.use(express.static("public"));
-server.set("view engine", "njk");
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server,
-  noCache: true,
-  autoescape: false,
-});
+    express: server,
+    noCache: true,
+    autoescape: false,
+})
 
 server.get("/", (requisition, response) => {
-  return response.redirect("/home");
-});
+    return response.redirect("/home")
+})
 
 server.get("/home", (requisition, response) => {
-  return response.render("index", { recipes: data });
-});
+    return response.render("index", { recipes: data })
+})
 
 server.get("/recipes", (requisition, response) => {
-  return response.render("recipes", { recipes: data });
-});
+    return response.render("recipes", { recipes: data })
+})
 
 server.get("/recipes/:index", (requisition, response) => {
-  const index = requisition.params.index;
-  const recipe = data[index];
+    const { id } = requisition.params.id
+    const recipe = data.find(recipe => recipe.id == id)
 
-  return response.render("recipes", { recipes: recipe });
-});
+    return response.render("recipes", { recipes: recipe })
+})
 
 server.listen(3000, () => {
-  console.log("Server is on!");
-});
+    console.log("Server is on!")
+})
