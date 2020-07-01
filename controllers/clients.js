6aa7c0324531ca5321377e55539ba16334ae8ff4
Silva404 <brenosilva0252@gmail.com>
2020-06-30 23:45:20 -0300

    ta bom por hoje

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,20 +1,21 @@
-const data = require('../public/scripts/data')
+// const data = require('../public/scripts/data')
+const data = require('../data.json')
 
 exports.index = (requisition, response) => {
     return response.redirect("/home")
 }
 
 exports.home = (requisition, response) => {
-    return response.render("clients/index", { recipes: data })
+    return response.render("clients/index", { recipes: data.recipes })
 }
 
 exports.recipes = (requisition, response) => {
-    return response.render("clients/recipes", { recipes: data })
+    return response.render("clients/recipes", { recipes: data.recipes })
 }
 
 exports.recipe = (requisition, response) => {
     const id = requisition.params.index
-    const recipe = data.find(recipe => recipe.id == id)
+    const recipe = data.recipes.find(recipe => recipe.id == id)
     if (!recipe) return response.send("Recipe not found!")
 
     return response.render("clients/recipe", { recipes: recipe })
---
+++ public/scripts/fields.js
@@ -0,0 +1,18 @@
+function addIngredient() {
+    const ingredients = document.querySelector("#ingredients");
+    const fieldContainer = document.querySelectorAll(".ingredient");
+
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    ingredients.appendChild(newField);
+}
+
+document
+    .querySelector(".add-ingredient")
+    .addEventListener("click", addIngredient);
