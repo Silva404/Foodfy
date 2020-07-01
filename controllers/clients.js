5b69c717176e5b6fe8457d032eff5b7dc008fb85
Silva404 <brenosilva0252@gmail.com>
2020-06-30 23:24:42 -0300

    general db is back

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,4 +1,4 @@
-const data = require('../data')
+const data = require('../public/scripts/data')
 
 exports.index = (requisition, response) => {
     return response.redirect("/home")
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const routes = express.Router()
 const clients = require('./controllers/clients')
 const recipes = require('./controllers/admin')
 
+//general
 routes.get("/", clients.index)
 routes.get("/home", clients.home)
 routes.get("/recipes", clients.recipes)
@@ -11,11 +12,11 @@ routes.get("/recipes/:index", clients.recipe)
 
 //admin
 routes.get("/admin/recipes", recipes.index)
-routes.post("/admin/recipes", recipes.post)
 routes.get("/admin/recipes/create", recipes.create)
 routes.get("/admin/recipes/:id", recipes.show)
 routes.get("/admin/recipes/:id/edit", recipes.edit)
 
+routes.post("/admin/recipes", recipes.post)
 routes.put("/admin/recipes", recipes.put)
 routes.delete("/admin/recipes", recipes.delete)
 
