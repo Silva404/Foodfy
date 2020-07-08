3c39f00ad5b45a29193b03b52f72c4be142dfb66
Silva404 <brenosilva0252@gmail.com>
2020-07-08 15:20:41 -0300

    today

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,9 +2,33 @@ const currentPage = location.pathname
 const menuItems = document.querySelectorAll('header .links a')
 const div = document.createElement('div')
 div.className = 'page'
+console.log(currentPage)
 
 for (let item of menuItems) {
   if (currentPage.includes(item.getAttribute('href'))) {
     item.appendChild(div)
   }
 }
+
+
+const cards = document.querySelectorAll(".card")
+const view = document.querySelector('#view')
+
+if (currentPage.includes('/admin')) {
+  for (let card of view) {
+    card.addEventListener("click", (e) => {
+      const cardId = card.getAttribute('id')
+
+      window.location.href = `/admin/recipes/${cardId}`
+    });
+  }
+} else {
+  for (let card of cards) {
+    card.addEventListener("click", (e) => {
+      const cardId = card.getAttribute('id')
+
+      window.location.href = `/recipes/${cardId}`
+    });
+  }
+}
+
