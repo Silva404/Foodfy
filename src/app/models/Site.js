c500ca518afd9589d6fac661a01faab8524a1b47
Silva404 <brenosilva0252@gmail.com>
2020-07-09 16:34:10 -0300

    refactoring

--- src/app/models/Site.js
+++ src/app/models/Site.js
@@ -8,13 +8,6 @@ module.exports = {
             callback(results.rows)
         })
     },
-    find(id, callback) {
-        db.query(`SELECT * FROM recipes WHERE id = $1`, [id], (err, results) => {
-            if (err) throw `Database error: ${err}`
-
-            callback(results.rows[0])
-        })
-    },
     filter(filter, callback) {
         db.query(`SELECT * 
         FROM recipes 
@@ -22,6 +15,13 @@ module.exports = {
             if (err) throw `Database error: ${err}`
 
             callback(results.rows)
+        })
+    },
+    find(id, callback) {
+        db.query(`SELECT * FROM recipes WHERE id = $1`, [id], (err, results) => {
+            if (err) throw `Database error: ${err}`
+
+            callback(results.rows[0])
         })
     }
 }
