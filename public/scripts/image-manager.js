66a2aa6fea41ca8295c2872cbb97c2225b01b31f
Silva404 <brenosilva0252@gmail.com>
2020-07-27 13:57:38 -0300

    images manager

--- public/scripts/image-manager.js
+++ public/scripts/image-manager.js
@@ -1,12 +1,15 @@
 const PhotosUpload = {
   preview: document.querySelector('#photos-preview'),
   uploadLimit: 5,
+  files: [],
   handleFileInput(event) {
     const { files: fileList } = event.target
 
     if (this.hasLimit(event)) return
 
     Array.from(fileList).forEach(file => {
+      this.files.push(file)
+
       const reader = new FileReader()
 
       reader.onload = () => {
@@ -20,16 +23,25 @@ const PhotosUpload = {
 
       reader.readAsDataURL(file)
     })
+
+    this.getAllFiles()
   },
   getContainer(image) {
     const div = document.createElement('div')
 
     div.classList.add('photo')
-    div.onclick = () => alert('remover')
+    div.onclick = this.removePhoto
     div.appendChild(image)
     div.appendChild(this.getRemoveButton())
 
     return div
+  },
+  getAllFiles(){
+    const dataTransfer = new DataTransfer()
+
+    this.files.forEach(file => dataTrasnfer.files.item.add(file))
+
+    console.log(dataTransfer)
   },
   hasLimit(event) {
     const { files: fileList } = event.target
@@ -50,6 +62,10 @@ const PhotosUpload = {
     return button
   },
   removePhoto(event){
-    
+    const photoDiv = event.target.parentNode
+    const photoArray = Array.from(PhotosUpload.preview.children)
+    const index = photoArray.indexOf(photoDiv)
+
+    photoDiv.remove()
   }
 }
