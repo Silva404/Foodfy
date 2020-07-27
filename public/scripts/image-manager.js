7d71bafd35f6eae05b37e5113e2746c2746d3747
Silva404 <brenosilva0252@gmail.com>
2020-07-27 13:38:52 -0300

    images manager

---
+++ public/scripts/image-manager.js
@@ -0,0 +1,52 @@
+const PhotosUpload = {
+  preview: document.querySelector('#photos-preview'),
+  uploadLimit: 5,
+  handleFileInput(event) {
+    const { files: fileList } = event.target
+
+    if (this.hasLimit(event)) return
+
+    Array.from(fileList).forEach(file => {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const image = new Image()
+        image.src = String(reader.result)
+
+        const div = this.getContainer(image)
+
+        this.preview.appendChild(div)
+      }
+
+      reader.readAsDataURL(file)
+    })
+  },
+  getContainer(image) {
+    const div = document.createElement('div')
+
+    div.classList.add('photo')
+    div.onclick = () => alert('remover')
+    div.appendChild(image)
+    div.appendChild(this.getRemoveButton())
+
+    return div
+  },
+  hasLimit(event) {
+    const { files: fileList } = event.target
+
+    if (fileList.length > this.uploadLimit) {
+      alert('Você não pode por mais de 5 fotos')
+      event.preventDefault()
+      return true
+    }
+
+    return false
+  },
+  getRemoveButton(){
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+    button.innerHTML = "close"
+
+    return button
+  }
+}
