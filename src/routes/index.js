aa17b691fe982d7d5f4118bae4e1f7ec8ee3f36f
Silva404 <brenosilva0252@gmail.com>
2020-08-04 15:39:48 -0300

    refactoring

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,13 @@
 const express = require('express')
 const routes = express.Router()
-const multer = require('../app/middlewares/multer')
-
-const site = require('../app/controllers/site')
 
 const recipes = require('./recipes')
 const chefs = require('./chefs')
+const site = require('./site')
 
 routes.use('/admin/recipes', recipes)
 routes.use('/admin/chefs', chefs)
-
-//site
-routes.get("/", site.index)
-routes.get("/home", site.home)
-routes.get("/recipes", site.recipes)
-routes.get("/recipes/:id", site.recipe)
-
+routes.use(site)
 
 
 module.exports = routes
