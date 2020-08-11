b33f0ff99fcb1d6c76ac332d9b9a0255da4d1f9e
Silva404 <brenosilva0252@gmail.com>
2020-08-11 12:34:27 -0300

    users limitation

--- src/routes/chefs.js
+++ src/routes/chefs.js
@@ -4,16 +4,16 @@ const multer = require('../app/middlewares/multer')
 
 const chefs = require('../app/controllers/ChefsController')
 
-const { isAdmin, onlyUsers, isLoggedRedirectToList } 
-= require('../app/middlewares/session')
+const { isAdmin, onlyUsers, isLoggedRedirectToList }
+  = require('../app/middlewares/session')
 
-routes.get("/", onlyUsers, chefs.index)
-routes.get("/create", onlyUsers, isAdmin, chefs.create)
-routes.get("/:id", onlyUsers, chefs.show)
-routes.get("/:id/edit", onlyUsers, chefs.edit)
+routes.get("/", chefs.index)
+routes.get("/create", isAdmin, chefs.create)
+routes.get("/:id", chefs.show)
+routes.get("/:id/edit", isAdmin, chefs.edit)
 
-routes.post("/", onlyUsers, multer.array("chef_photos", 1), chefs.post)
-routes.put("/", onlyUsers, multer.array("chef_photos", 1), chefs.put)
+routes.post("/", multer.array("chef_photos", 1), chefs.post)
+routes.put("/", multer.array("chef_photos", 1), chefs.put)
 routes.delete("/", isAdmin, chefs.delete)
 
 module.exports = routes
